f440445ad163c65a4cb9506f577a5270a526d102
Jayesh Patil <jp7470484@gmail.com>
2023-08-27 16:35:32 +0530

    lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:review_app/constants/color.dart';
@@ -9,7 +10,9 @@ import 'package:review_app/routes/route_generator.dart';
 import 'features/reviews/presentation/pages/liked.dart';
 import 'features/reviews/presentation/pages/profile.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
