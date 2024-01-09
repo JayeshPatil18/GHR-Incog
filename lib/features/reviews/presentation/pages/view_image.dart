6066742e3218b327b3a0f23a0cf2ef3fd48d9951
Jayesh Patil <jp7470484@gmail.com>
2024-01-09 19:15:14 +0530

    all set

--- lib/features/reviews/presentation/pages/view_image.dart
+++ lib/features/reviews/presentation/pages/view_image.dart
@@ -24,7 +24,7 @@ class ViewImage extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       body: PhotoView(
-        imageProvider: isNetworkImage ? NetworkImage('https://i.insider.com/61e9ac1cda4bc600181aaf63?width=700') : FileImage(io.File(imageUrl)) as ImageProvider,
+        imageProvider: isNetworkImage ? NetworkImage(imageUrl) : FileImage(io.File(imageUrl)) as ImageProvider,
         minScale: PhotoViewComputedScale.contained * 0.8,
         maxScale: PhotoViewComputedScale.covered * 1.8,
         initialScale: PhotoViewComputedScale.contained,
