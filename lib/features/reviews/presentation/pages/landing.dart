8a03c3812e842fbd4500ad2d38058f8f315a627c
unknown <jp7470484@gmail.com>
2024-03-02 21:13:13 +0530

    done with closing of app

--- lib/features/reviews/presentation/pages/landing.dart
+++ lib/features/reviews/presentation/pages/landing.dart
@@ -44,108 +44,136 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<BottomNavigationProvider>(
-      builder: ((context, value, child) {
-        return Scaffold(
-          extendBody: true,
-          backgroundColor: Colors.transparent,
-          body: Container(
-              decoration: BoxDecoration(gradient: AppColors.mainGradient),
-              child: screens[value.currentIndex]),
-          resizeToAvoidBottomInset: false,
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: AppColors.secondaryColor10,
-            elevation: AppElevations.fabButtonElev,
-            child: Container(
-              width: 26,
-              height: 26,
-              decoration: BoxDecoration(
-      color: Colors.transparent,
-      borderRadius: BorderRadius.all(Radius.circular(50)),
-      boxShadow: [
-        BoxShadow(
-          color: AppColors.secondaryColor10.withOpacity(0.2),
-          spreadRadius: 3,
-          blurRadius: 3,
-          offset: Offset(0, 3),
-        ),
-      ],
-    ),
-              child: Image.asset('assets/icons/plus.png', color: AppColors.primaryColor30),
-            ),
-            onPressed: () {
-              if(MyApp.userId == -1){
-                loginRequiredObj.showLoginRequiredDialog(context);
-              } else{
-                Navigator.pushNamed(context, 'upload', arguments: TwoStringArg('-1', ''));
-              }
-            },
-          ),
-          bottomNavigationBar: ClipRRect(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0)),
-            child: BottomAppBar(
-              elevation: AppElevations.bottomNavBarElev,
-              shape: CircularNotchedRectangle(),
-              notchMargin: 12,
-              color: AppColors.gradientMid,
+    return WillPopScope(
+      onWillPop: () async {
+        final value = await showDialog<bool>(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text('Are you sure you want to exit?'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Yes, exit', style: TextStyle(color: Colors.grey)),
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                  ),
+                  TextButton(
+                    child: Text('No'),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
+                ],
+              );
+            }
+        );
+
+        return value == true;
+      },
+      child: Consumer<BottomNavigationProvider>(
+        builder: ((context, value, child) {
+          return Scaffold(
+            extendBody: true,
+            backgroundColor: Colors.transparent,
+            body: Container(
+                decoration: BoxDecoration(gradient: AppColors.mainGradient),
+                child: screens[value.currentIndex]),
+            resizeToAvoidBottomInset: false,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: AppColors.secondaryColor10,
+              elevation: AppElevations.fabButtonElev,
               child: Container(
-                height: 65,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+                width: 26,
+                height: 26,
+                decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.secondaryColor10.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+                child: Image.asset('assets/icons/plus.png', color: AppColors.primaryColor30),
+              ),
+              onPressed: () {
+                if(MyApp.userId == -1){
+                  loginRequiredObj.showLoginRequiredDialog(context);
+                } else{
+                  Navigator.pushNamed(context, 'upload', arguments: TwoStringArg('-1', ''));
+                }
+              },
+            ),
+            bottomNavigationBar: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0)),
+              child: BottomAppBar(
+                elevation: AppElevations.bottomNavBarElev,
+                shape: CircularNotchedRectangle(),
+                notchMargin: 12,
+                color: AppColors.gradientMid,
+                child: Container(
+                  height: 65,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
 
-                    // Home
-                    GestureDetector(
-                      child: bottomNavIcons[0],
-                      onTap: () {
-                        value.updateIndex(0);
-                      },
-                    ),
+                      // Home
+                      GestureDetector(
+                        child: bottomNavIcons[0],
+                        onTap: () {
+                          value.updateIndex(0);
+                        },
+                      ),
 
-                    // Liked
-                    GestureDetector(
-                      child: bottomNavIcons[1],
-                      onTap: () {
-                        if(MyApp.userId == -1){
-                          loginRequiredObj.showLoginRequiredDialog(context);
-                        } else{
-                          value.updateIndex(1);
-                        }
-                      },
-                    ),
+                      // Liked
+                      GestureDetector(
+                        child: bottomNavIcons[1],
+                        onTap: () {
+                          if(MyApp.userId == -1){
+                            loginRequiredObj.showLoginRequiredDialog(context);
+                          } else{
+                            value.updateIndex(1);
+                          }
+                        },
+                      ),
 
-                    SizedBox(width: 80.0), // Empty space for center notch
+                      SizedBox(width: 80.0), // Empty space for center notch
 
-                    // Leaderboard
-                    GestureDetector(
-                      child: bottomNavIcons[2],
-                      onTap: () {
-                        value.updateIndex(2);
-                      },
-                    ),
+                      // Leaderboard
+                      GestureDetector(
+                        child: bottomNavIcons[2],
+                        onTap: () {
+                          value.updateIndex(2);
+                        },
+                      ),
 
-                    // Profile
-                    GestureDetector(
-                      child: bottomNavIcons[3],
-                      onTap: () {
-                        if(MyApp.userId == -1){
-                          loginRequiredObj.showLoginRequiredDialog(context);
-                        } else{
-                          value.updateIndex(3);
-                        }
-                      },
-                    ),
-                  ],
+                      // Profile
+                      GestureDetector(
+                        child: bottomNavIcons[3],
+                        onTap: () {
+                          if(MyApp.userId == -1){
+                            loginRequiredObj.showLoginRequiredDialog(context);
+                          } else{
+                            value.updateIndex(3);
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        );
-      }),
+          );
+        }),
+      ),
     );
   }
 }
