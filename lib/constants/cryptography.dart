4e8a42f52c5867149bdcc2126f30c95724ae3cb3
unknown <jp7470484@gmail.com>
2024-03-06 18:49:14 +0530

    done risk

--- lib/constants/cryptography.dart
+++ lib/constants/cryptography.dart
@@ -1,24 +1,32 @@
 import 'package:encrypt/encrypt.dart';
 
 class CryptographyConfig{
-  static String keyString = 'my 32 length key................';
+  static String keyString = 'ThisIsNotKeyThisIsStringThatCanRevealIdentityOfHackerHaHaWhoHasLengthOf400';
   static final key = Key.fromUtf8(keyString);
-  static final iv = IV.fromLength(16);
+  static final iv = IV.fromLength((keyString.length/2).toInt() ?? 37);
 
   static String encryptText(String plainText, Key key, IV iv) {
-    final encrypter = Encrypter(AES(key));
+    try{
+      final encrypter = Encrypter(AES(key));
 
-    final encrypted = encrypter.encrypt(plainText, iv: iv);
+      final encrypted = encrypter.encrypt(plainText, iv: iv);
 
-    return encrypted.base64;
+      return encrypted.base64;
+    } catch(e){
+      return plainText;
+    }
   }
 
   static String decryptText(String encryptedText, Key key, IV iv) {
-    final encrypter = Encrypter(AES(key));
+    try{
+      final encrypter = Encrypter(AES(key));
 
-    final encrypted = Encrypted.fromBase64(encryptedText);
-    final decrypted = encrypter.decrypt(encrypted, iv: iv);
+      final encrypted = Encrypted.fromBase64(encryptedText);
+      final decrypted = encrypter.decrypt(encrypted, iv: iv);
 
-    return decrypted;
+      return decrypted;
+    } catch(e){
+      return '';
+    }
   }
 }
